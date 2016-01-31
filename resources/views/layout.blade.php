c633deb00f82242d5359d23cf06118c53c670d90
Qnk <jaqnk10@gmail.com>
2016-01-31 15:05:09 +0100

    Fix 404 style.css file not found when upload an image

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -2,7 +2,7 @@
 <html>
     <head>
         <title>Laravel</title>
-        <link href="css/style.css" rel="stylesheet" type="text/css">
+        <link href="{{ URL::to('/') }}/css/style.css" rel="stylesheet" type="text/css">
         <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet" type="text/css">
         <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
         <link rel="stylesheet" type="text/css" href="{{ URL::to('/') }}/css/libs/sweetalert.css">
