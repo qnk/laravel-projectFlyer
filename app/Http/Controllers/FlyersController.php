e845fed601442fddd3c0824df765f5e4aa6d873f
Qnk <jaqnk10@gmail.com>
2016-02-01 11:32:38 +0100

    Add only registered users can do CRUD operations on flyers but non registered users are able to view a flyer

--- app/Http/Controllers/FlyersController.php
+++ app/Http/Controllers/FlyersController.php
@@ -15,7 +15,7 @@ class FlyersController extends Controller
 {
     public function __construct()
     {
-        //$this->middleware('auth');
+        $this->middleware('auth',['except' => 'show']);
 
         //parent::__construct();
     }
