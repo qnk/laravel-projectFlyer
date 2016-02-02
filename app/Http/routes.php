631499178b74ddf1f8dd63e9cb54258f916075c1
Qnk <jaqnk10@gmail.com>
2016-02-02 11:16:06 +0100

    Give an alias to store a photo route. Create new Photo class in order to follow a more RESTfull way when storing new photos.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,10 +5,18 @@ use Illuminate\Http\Request;
 Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
+	// usage inside a laravel route
+	Route::get('/home', function()
+	{
+	    $img = Image::make(URL::to('/').'/flyers/photos/1454321308imagesCACQQ84G.jpg')->resize(300, 200);
+
+	    return $img->response('jpg');
+	});
+
     Route::get('/', 'PagesController@home');
 
 	Route::resource('flyers','FlyersController');	
 
 	Route::get('{zip}/{street}','FlyersController@show');
-	Route::post('{zip}/{street}/photos','FlyersController@addPhoto');
+	Route::post('{zip}/{street}/photos',['as' => 'store_photo_path' => 'PhotosController@store']);
 });
--- app/Photo.php
+++ app/Photo.php
@@ -11,9 +11,66 @@ class Photo extends Model
 	protected $fillable = ['path'];
 
 	protected $baseDir = '/flyers/photos';
+
+	protected $file;
     
     public function flyer()
     {
     	return $this->belongsTo('App\Flyer');
+    }
+
+    public function fromFile(UploadedFile $file)
+    {
+    	$photo = new static;
+
+    	$photo->file = $file;
+
+    	return $photo->fill([
+    		'name'           => $photo->fileName(),
+    		'path'           => $photo->filePath(),
+    		'thumbnail_path' => $photo->thumnailPath()
+    	]);
+    }
+
+    public function filePath()
+    {
+    	return $this->baseDir() . '/' . $this->fileName();
+    }
+
+    public function thumbnailPath()
+    {
+    	return $this->baseDir() . '/tb-' . $this->fileName();
+    }
+
+    public function baseDir()
+    {
+    	return 'flyers/photos';
+    }
+
+    public function upload(UploadedFile $file)
+    {
+    	$this->file->move($this->baseDir(), $this->fileName());
+
+    	$this->makeThumbnail();
+
+    	return $this;
+    }
+
+    protected function makeThumbnail()
+    {
+    	Image::make($this->filePath())
+    		->fit(200)
+    		->save($this->thumbnailPath());
+    }
+
+    public function fileName()
+    {
+    	$name = sha1(
+    		time() . $this->file->getClientOriginalName();
+    	);
+
+    	$extension = $this->file->getClientOriginalExtension();
+
+    	return "{$name}/{$extension}";
     }
 }
