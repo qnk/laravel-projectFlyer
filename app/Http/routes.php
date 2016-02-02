97a19cf2d6fc235daadd9abdf32b2bd0c93f663d
Qnk <jaqnk10@gmail.com>
2016-02-02 17:21:40 +0100

    Remove old test routes

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -6,8 +6,7 @@ Route::group(['middleware' => 'web'], function () {
     Route::auth();
 
     Route::get('/', 'PagesController@home');
-
-	Route::resource('flyers','FlyersController');	
+s	Route::resource('flyers','FlyersController');	
 
 	Route::get('{zip}/{street}','FlyersController@show');
 	Route::post('{zip}/{street}/photos',['as' => 'store_photo_path','uses' => 'PhotosController@store']);
